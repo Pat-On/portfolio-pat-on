fbf737dc5196e76e0a69b61a1697f5cbdd1d428b
PatOn <mahomed2278@gmail.com>
2021-04-07 20:57:53 +0100

    base set up done

--- src/App.js
+++ src/App.js
@@ -13,18 +13,21 @@ import Layout from "./hoc/Layout/Layout";
 import "./App.css";
 
 function App() {
+  //temporary added DIV to reuse old style from react
   return (
-    <Layout className="App">
-      <Switch>
-        <Route path="/about" render={(props) => <About {...props} />} />
-        <Route path="/skills" render={(props) => <MySkills {...props} />} />
-        <Route path="/projects" render={(props) => <Projects {...props} />} />
-        <Route path="/contact" render={(props) => <Contact {...props} />} />
-        <Route path="/resume" render={(props) => <Resume {...props} />} />
-        <Route path="/" exact component={HomePage} />
-        <Redirect to="/" />
-      </Switch>
-    </Layout>
+    <div className="App">
+      <Layout>
+        <Switch>
+          <Route path="/about" render={(props) => <About {...props} />} />
+          <Route path="/skills" render={(props) => <MySkills {...props} />} />
+          <Route path="/projects" render={(props) => <Projects {...props} />} />
+          <Route path="/contact" render={(props) => <Contact {...props} />} />
+          <Route path="/resume" render={(props) => <Resume {...props} />} />
+          <Route path="/" exact component={HomePage} />
+          <Redirect to="/" />
+        </Switch>
+      </Layout>
+    </div>
   );
 }
 
