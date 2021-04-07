31ac7452f6c881718753bb7adbd180048e1619bd
PatOn <mahomed2278@gmail.com>
2021-04-07 19:50:15 +0100

    added base frame

---
+++ src/components/homePage/homePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HomePage = (props) => {
+  // temporary
+
+  return (
+    <div>
+      <h1>Hi, I'm PATRYK, web developer</h1>
+      <h2>Seeking The Next Exciting Challenge</h2>
+    </div>
+  );
+};
+
+export default HomePage;
