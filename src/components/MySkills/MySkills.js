f8e6bad8cf644451e166831583ae567927b9e364
PatOn <mahomed2278@gmail.com>
2021-04-08 20:52:56 +0100

    added icons to soft skills

--- src/components/MySkills/MySkills.js
+++ src/components/MySkills/MySkills.js
@@ -12,13 +12,22 @@ import python3 from "../../assets/icons/python3.png";
 import reactLogo from "../../assets/icons/react.png";
 import reduxLogo from "../../assets/icons/redux.png";
 
+import speakerIcon from "../../assets/icons/speakerIcon.png";
+import communication from "../../assets/icons/communication.png";
+import project from "../../assets/icons/project.png";
+import organisational from "../../assets/icons/organisational.png";
+import leadership from "../../assets/icons/leadership.png";
+import problemSolving from "../../assets/icons/problemSolving.png";
+import creativity from "../../assets/icons/creativity.png";
+import analyticalThinking from "../../assets/icons/analyticalThinking.png";
+import attentionToDetails from "../../assets/icons/attentionToDetails.png";
+
 const MySkills = () => {
   return (
     <div className={classes.aboutPageContainer}>
       <h1>My Skills</h1>
       <h2>Hard Skills</h2>
       <h3>Recently used technologies by me:</h3>
-
       <ul className={classes.unorderedList}>
         <li>
           <img className={classes.logo} src={jsLogo} alt="js logo" />
@@ -58,37 +67,107 @@ const MySkills = () => {
           <p> Node.js</p>
         </li>
       </ul>
-
-      <p style={{ color: "red" }}>Think over it</p>
-
       <h2>Soft Skills</h2>
       <p>
         In my academic career I had a chance to be a member of the teams, where
         we were working together on many different types of project. Most of
         them were focused on volunteer-social projects, science projects and
-        cultural project. <br /> It was great chance to learn to work
-        effectively under the pressure and to get better understanding of other
-        people. It was great chance to develop my interpersonal and soft skills.
-        My progress in this field gave me chance to managing and supporting my
-        colleagues, what was great chance to test my soft skills. <br /> During
-        my post academic experience I was traveling around the Europe getting to
-        know many different great people and gaining experience in
+        cultural project.
+      </p>
+      <p>
+        It was great chance to learn to work effectively under the pressure and
+        to get better understanding of other people. It was great chance to
+        develop my interpersonal and soft skills. My progress in this field gave
+        me chance to managing and supporting my colleagues, what was great
+        chance to test my soft skills.
+      </p>
+      <p>
+        During my post academic experience I was traveling around the Europe
+        getting to know many different great people and gaining experience in
         communication. My recent experience to work in customer service sector
         gave me the great chance to polish my soft skills in different role,
         what was great experience.
       </p>
+      <h3>Some chosen softs skills:</h3>
+      <ul className={classes.unorderedList}>
+        <li>
+          <img
+            className={classes.logo}
+            src={analyticalThinking}
+            alt="Icon made by www.wishforge.games from www.freeicons.io"
+          />
+          <p>Analytical thinking</p>
+        </li>
+        <li>
+          <img
+            className={classes.logo}
+            src={problemSolving}
+            alt="Icon made by MD Badsha Meah from www.freeicons.io"
+          />
+          <p>Problem solving </p>
+        </li>
 
-      <ul>
-        <li>Public speaking</li>
-        <li>Communication</li>
-        <li>Project management</li>
-        <li>Organisational</li>
-        <li>Leadership</li>
-        <li>Problem solving </li>
-        <li>Creativity</li>
-        <li>Analytical thinking</li>
-        <li>Attention to Detail</li>
+        <li>
+          <img
+            className={classes.logo}
+            src={attentionToDetails}
+            alt="Icon made by icon king1 from www.freeicons.io"
+          />
+          <p>Attention to Detail</p>
+        </li>
+        <li>
+          <img
+            className={classes.logo}
+            src={speakerIcon}
+            alt="Icon made by icon king1 from www.freeicons.io"
+          />
+          <p>Public speaking</p>
+        </li>
+        <li>
+          <img
+            className={classes.logo}
+            src={organisational}
+            alt="Icon made by ColourCreatype from www.freeicons.io"
+          />
+          <p>Organisational</p>
+        </li>
+
+        <li>
+          <img
+            className={classes.logo}
+            src={project}
+            alt="Icon made by icon king1 from www.freeicons.io"
+          />
+          <p>Project management</p>
+        </li>
+
+        <li>
+          <img
+            className={classes.logo}
+            src={leadership}
+            alt="Icon made by MD Badsha Meah from www.freeicons.io"
+          />
+          <p>Leadership</p>
+        </li>
+
+        <li>
+          <img
+            className={classes.logo}
+            src={creativity}
+            alt="Icon made by MD Badsha Meah from www.freeicons.io"
+          />
+          <p>Creativity</p>
+        </li>
+        <li>
+          <img
+            className={classes.logo}
+            src={communication}
+            alt="Icon made by MD Badsha Meah from www.freeicons.io"
+          />
+          <p>Communication</p>
+        </li>
       </ul>
+      <p></p>
     </div>
   );
 };
--- src/components/about/about.js
+++ src/components/about/about.js
@@ -15,24 +15,20 @@ const About = () => {
       <p>
         I'm a full stack developer who started from non IT background. I came
         from academic sector, where during my ph.d. studies I learn
-        methodological and analytical approach to solve problems.
+        methodological and analytical approach to solving problems.
       </p>
 
       <p>
         I have appetite of curiosity in new technologies, which lead to my
         persistence and enjoyment to learn new things which then enable me to
-        teach others, something I enjoy doing as well. (My experience in
-        teaching and great curiosity, with persistence is giving me chance to
-        enjoy getting to know new technologies and to learn new things.){" "}
+        teach others, something I enjoy doing as well.
       </p>
 
       <p>
         I am a well organized person, who able to work independently as well as
         in a team. This reflect in the achievement of my past career, where my
         team and I always produced high standards result and achieved the goal
-        on time. (Well-organised person, who can work independently, but I'm as
-        well good team player. During my career me and my team were always
-        achieving high goals and standards.)
+        on time.
       </p>
 
       <p>
@@ -40,17 +36,11 @@ const About = () => {
         Science and Higher Education of Republic of Poland (only 1000 students
         being awarded) twice during my studies at university. To achieve this, I
         set and focused on the goal, plan my time well and able to work under
-        pressure. [I am result oriented and work well under pressure in order to
-        achieve goals to tight deadline. That is why during my studies at
-        University I received two times Grant Of Ministry of Science and Higher
-        Education of Republic of Poland, which is granted to maximum 1 000
-        students in Poland for academic year. ]
+        pressure.
       </p>
       <p>
         I'm interested to find the work IT sector, I look forward to work with a
-        dynamic team full of positive people and the challenges. [where I will
-        have chance to challenge my self and to work with a great dynamic team
-        full of positive people.]
+        dynamic team full of positive people and the challenges.
       </p>
     </div>
   );
