dbfa414bafb2a3c500d377bc92030cf82ccf20ce
PatOn <mahomed2278@gmail.com>
2021-04-10 22:08:31 +0100

    language p.2

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -6,10 +6,13 @@ const Resume = () => {
   return (
     <div className={classes.resumePageContainer}>
       <h1 className={classes.resumeH1}>RESUME</h1>
-      <p>
+      {/* <p>
         <b>Thank You</b>
+      </p> */}
+      <p>
+        Thank you for your time and interest. Please feel free to have a look at
+        my CV using the link below.
       </p>
-      <p>for your time and showed interest to get know something about me.</p>
 
       <a href="https://drive.google.com/u/0/uc?id=1gOU1dMzAkWQmzc2m-B8a6zc22XSlp7DC&export=download">
         <button className={classes.myButton}>Find My CV HERE</button>
